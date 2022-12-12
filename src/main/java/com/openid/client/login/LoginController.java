b285eebb0ff97ddd3e2f209ed0e2686a0fa65453
berrinho <joe.berry@gmail.com>
2022-12-12 09:24:15 +0000

    Tweaked LoginController to show claims both default and user profile

--- src/main/java/com/openid/client/login/LoginController.java
+++ src/main/java/com/openid/client/login/LoginController.java
@@ -1,25 +1,37 @@
 package com.openid.client.login;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/userportal")
-    public String getUserPortal(Model model) {
-        System.out.println(" in controller" );
+    public String getUserPortal(@RequestParam Map<String,String> allRequestParams, Model model,
+                                @AuthenticationPrincipal DefaultOidcUser defaultOidcUser) {
 
+        allRequestParams.values().stream().forEach(  System.out::println  );
+
+        System.out.println(" model"  + model.toString());
 
         Object principal = SecurityContextHolder.getContext()
                 .getAuthentication().getPrincipal();
 
-// put the UserDetails object here.
         model.addAttribute("userDetails", principal);
+
+        model.addAttribute("claims", defaultOidcUser.getClaims());
+
+        Map<String, Object> attributes = defaultOidcUser.getAttributes();
+
         return "userportal";
     }
 
